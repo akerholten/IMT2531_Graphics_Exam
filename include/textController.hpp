b2b5843792010b693fb8afd96ce54f203e7414a3
Nikolai Åkerholt <nikolai_aakk@hotmail.com>
2018-05-13 03:10:18 +0200

    Freetype now loading successfully

--- include/textController.hpp
+++ include/textController.hpp
@@ -2,10 +2,21 @@
 
 //#include "freetype/config/ftconfig.h"
 //#include "freetype/config/ftoption.h"
+//#include "freetype/freetype.h"
 #include <ft2build.h>
-#include "freetype/freetype.h"
 #include FT_FREETYPE_H 
 #include <iostream>
+
+#include <map>
+#include "glm.hpp"
+#include "GL/glew.h"
+
+struct Character {
+	GLuint     TextureID;  // ID handle of the glyph texture
+	glm::ivec2 Size;       // Size of glyph
+	glm::ivec2 Bearing;    // Offset from baseline to left/top of glyph
+	GLuint     Advance;    // Offset to advance to next glyph
+};
 
 class TextController {
 public:
@@ -13,5 +24,6 @@ public:
 	void init();
 
 private:
+	std::map<GLchar, Character> Characters;
 
 };
